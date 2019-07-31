df89751ffecafee588744c808807db6c9aaa80a3
Shubhamnb <bohari2@gmail.com>
2019-07-31 19:16:29 +0530

    some

--- NoteMicro/src/main/java/com/note/micro/service/NoteServiceImpl.java
+++ NoteMicro/src/main/java/com/note/micro/service/NoteServiceImpl.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NoteServiceImpl implements NoteService {
+private void sout() {
+	// TODO Auto-generated method stub
 
+}
 
 
 }
